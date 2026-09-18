0034f4aed678bfe23c770bfd92267528c7b971a6
Matthew W <tesla3698@gmail.com>
2026-09-18 16:45:19 -0400

    renamed sign in to signin

--- frontend/settings/settings.js
+++ frontend/settings/settings.js
@@ -94,7 +94,7 @@
         .forEach((k) => localStorage.removeItem(k));
     } catch (e) { /* storage unavailable */ }
     clearModal.close();
-    window.location.href = '../sign in/signin.html';
+    window.location.href = '../signin/signin.html';
   });
 
   /* ---------- Delete account (danger zone: wipe + sign out + redirect) ---------- */
@@ -111,13 +111,13 @@
         .forEach((k) => localStorage.removeItem(k));
     } catch (e) { /* storage unavailable */ }
     if (BW.signOut) BW.signOut();
-    window.location.href = '../sign in/signin.html';
+    window.location.href = '../signin/signin.html';
   });
 
   /* ---------- Sign out ---------- */
   document.getElementById('signOutBtn').addEventListener('click', () => {
     if (BW.signOut) BW.signOut();
-    window.location.href = '../sign in/signin.html';
+    window.location.href = '../signin/signin.html';
   });
 
   renderFromSettings();
